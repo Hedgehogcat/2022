bdf19572b1cb7621b4aebb9cf09bf85dc0e6574b
copyanger <copyanger@163.com>
2022-07-08 19:16:23 +0800

    忽略 bin 下debug release

--- GrpcServiceDemo/WebApplication1/Controllers/HomeController.cs
+++ GrpcServiceDemo/WebApplication1/Controllers/HomeController.cs
@@ -42,5 +42,27 @@ namespace WebApplication1.Controllers
                 return ex.Message;
             }
         }
+        [HttpGet]
+        public async Task<string> Get456()
+        {
+            try
+            {
+                //使用http
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+                string url = "http://192.168.13.129:8090";
+                using (var channel = GrpcChannel.ForAddress(url))
+                {
+                    var client = new Greeter.GreeterClient(channel);
+                    var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+
+                    Console.WriteLine($"结果:message:{reply.Message}");
+                    return reply.Message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
     }
 }
